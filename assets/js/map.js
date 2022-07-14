d0b9fef755a2756f55c17cede4faf74b67a2da8b
SaraStoimenovska <sara.stoimenovska@yahoo.com>
2022-07-14 16:48:57 +0200

    Fixed small remarks

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,18 @@
-function onSvgLoad() {
-    var belowGround = document.getElementById('Below_ground_gas');
-    belowGround.style.visibility = 'hidden'
-    var aboveGround = document.getElementById('Above_ground_Gas');
-    aboveGround.style.visibility = 'hidden'
-    var emerganyGasValves = document.getElementById('Emergany_Gas_Isolation_valves');
-    emerganyGasValves.style.visibility = 'hidden'
-  }
+function onSvgOsbourneLoad() {
+  var belowGround = document.getElementById('Below_ground_gas');
+  belowGround.style.visibility = 'hidden'
+  var aboveGround = document.getElementById('Above_ground_Gas');
+  aboveGround.style.visibility = 'hidden'
+  var emerganyGasValves = document.getElementById('Emergany_Gas_Isolation_valves');
+  emerganyGasValves.style.visibility = 'hidden'
+}
+
+function onSvgFalkinLoad() {
+  var belowGround = document.getElementById('Below_ground_gas_pipeline');
+  belowGround.style.visibility = 'hidden'
+  var aboveGround = document.getElementById('Above_ground_gas_pipeline');
+  aboveGround.style.visibility = 'hidden'
+}
   
   function toggleLayer(id, btn) {
     var element = document.getElementById(id);
